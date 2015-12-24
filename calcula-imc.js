db7c84e651e4870e17ca9da84033235471886265
juliafealves <juliafealves@gmail.com>
2015-12-23 23:41:26 -0800

    Incluído código para evitar divisão por zero.

--- calcula-imc.js
+++ calcula-imc.js
@@ -1,6 +1,10 @@
 var peso = 100;
 var altura = 2.00;
 var paciente = "Júlia Fernandes";
-var imc = peso / (altura * altura);
 
+if(altura > 0){
+  var imc = peso / (altura * altura);
+}else {
+  console.log("Não foi possível calcular o IMC, pois a altura não é um valor válido").
+}
 console.log(imc);
