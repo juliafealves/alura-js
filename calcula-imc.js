2cb7d1a7a7f3ce5bb39cf4780a9bc1942a783880
juliafealves <juliafealves@gmail.com>
2015-12-29 23:56:04 -0800

    Refatorado código utilizando objeto Paciente. Acesso via DOM dos conteúdos do elemento html.

--- calcula-imc.js
+++ calcula-imc.js
@@ -1,10 +1,14 @@
-var peso = 100;
-var altura = 2.00;
-var paciente = "Júlia Fernandes";
+var paciente = {
+                nome    : document.getElementById('nome-1').textContent,
+                peso    : document.getElementById('peso-1').textContent,
+                altura  : document.getElementById('altura-1').textContent
+              };
 
-if(altura > 0){
-  var imc = peso / (altura * altura);
-}else {
-  console.log("Não foi possível calcular o IMC, pois a altura não é um valor válido").
+if(paciente.altura > 0){
+  var imc = paciente.peso / (paciente.altura * paciente.altura);
+  document.getElementById('imc-1').textContent = imc;
+}else{
+  console.log("Não foi possível calcular o IMC, pois a altura não é um valor válido");
 }
+
 console.log(imc);
