533344630260abbf86377b635977c5f09a9297c8
juliafealves <juliafealves@gmail.com>
2016-01-09 20:41:40 -0800

    Refatorado código para pegar dinamicamente dados da tabela html, gerando objeto paciente e calculando imc com auxílio de loop for.

--- calcula-imc.js
+++ calcula-imc.js
@@ -1,14 +1,20 @@
-var paciente = {
-                nome    : document.getElementById('nome-1').textContent,
-                peso    : document.getElementById('peso-1').textContent,
-                altura  : document.getElementById('altura-1').textContent
-              };
+var trPacientes = document.getElementsByClassName('paciente');
 
-if(paciente.altura > 0){
-  var imc = paciente.peso / (paciente.altura * paciente.altura);
-  document.getElementById('imc-1').textContent = imc;
-}else{
-  console.log("Não foi possível calcular o IMC, pois a altura não é um valor válido");
+for(var posicaoAtual = 0; trPacientes.length - 1; posicaoAtual++){
+  var nome = trPacientes[posicaoAtual].getElementsByClassName('info-nome')[0].textContent;
+  var peso = trPacientes[posicaoAtual].getElementsByClassName('info-peso')[0].textContent;
+  var altura = trPacientes[posicaoAtual].getElementsByClassName('info-altura')[0].textContent;
+
+  var paciente = {
+                  nome    : nome,
+                  peso    : peso,
+                  altura  : altura
+                };
+
+  if(paciente.altura > 0){
+    var imc = paciente.peso / (paciente.altura * paciente.altura);
+    trPacientes[posicaoAtual].getElementsByClassName('info-imc')[0].textContent = imc;
+  }else{
+    console.log("Não foi possível calcular o IMC, pois a altura não é um valor válido");
+  }
 }
-
-console.log(imc);
