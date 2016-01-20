a29c54eef7260d68978e387fd482af556e5c863e
juliafealves <juliafealves@gmail.com>
2016-01-19 16:38:19 -0800

    Adicionado evento onclick ao botão "Calcular IMCs".

--- calcula-imc.js
+++ calcula-imc.js
@@ -1,6 +1,12 @@
-var trPacientes = document.getElementsByClassName('paciente');
+var botao = document.getElementById('calcula-imcs');
+/**
+ * Faz o cálculo de todos os IMCs da tabela.
+ */
+botao.addEventListener('click', function(){
+    var trPacientes = document.getElementsByClassName('paciente');
 
-percorreArray(trPacientes, function(trPacienteAtual){
-    var paciente = montaPaciente(trPacienteAtual);
-    trPacienteAtual.getElementsByClassName('info-imc')[0].textContent = paciente.getIMC();
+    percorreArray(trPacientes, function (trPacienteAtual) {
+        var paciente = montaPaciente(trPacienteAtual);
+        trPacienteAtual.getElementsByClassName('info-imc')[0].textContent = paciente.getIMC();
+    });
 });
